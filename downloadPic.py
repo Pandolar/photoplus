718f4ece96561d4dfbb88d7a54a79e6c6331619f
Pandolar <54896726+Pandolar@users.noreply.github.com>
2025-01-14 10:52:28 +0800

    Update downloadPic.py 中文提示

--- downloadPic.py
+++ downloadPic.py
@@ -1,9 +1,10 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-# __Author__ = "Michael Yu"
-# Filename: downloadPic.py
-# Date: 2023/6/25
-
+# python3
+# -- coding: utf-8 --
+# -------------------------------
+# @Author : Terry
+# @File : down.py
+# @Time : 2024/2/5 10:34
+# -------------------------------
 import os
 import time
 import requests
@@ -12,17 +13,13 @@ from operator import itemgetter
 
 # 常量
 SALT = 'laxiaoheiwu'
-COUNT = 9999
+DEFAULT_COUNT = 9999
 
 # 定义请求的参数
-data = {
-    'activityNo': 0,
+BASE_PARAMS = {
     'isNew': False,
-    'count': COUNT,
-    'page': 1,
     'ppSign': 'live',
     'picUpIndex': '',
-    '_t': 0,
 }
 
 # 对象按键排序
@@ -38,65 +35,83 @@ def md5(value):
     return m.hexdigest()
 
 # 获取所有图片
-def get_all_images(id,place):
-    image_path = "../Pics/" + str(place)
-    if not os.path.exists(image_path):
-        os.makedirs(image_path)
-    t = int(time.time() * 1000)
-    data['activityNo'] = id
-    data['_t'] = t
-    data_sort = obj_key_sort(data)
-    sign = md5(data_sort + SALT)
+def get_all_images(activity_no, save_path):
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+
+    timestamp = int(time.time() * 1000)
     params = {
-        **data,
-        '_s': sign,
-        'ppSign': 'live',
-        'picUpIndex': '',
+        **BASE_PARAMS,
+        'activityNo': activity_no,
+        'count': DEFAULT_COUNT,
+        'page': 1,
+        '_t': timestamp,
     }
+
+    # 生成签名
+    sorted_params = obj_key_sort(params)
+    sign = md5(sorted_params + SALT)
+    params['_s'] = sign
+
     try:
-        res = requests.get('https://live.photoplus.cn/pic/pics', params=params)
-        res.raise_for_status()  # 如果响应状态不是200，就主动抛出异常
+        response = requests.get('https://live.photoplus.cn/pic/pics', params=params)
+        response.raise_for_status()  # 如果响应状态不是200，就主动抛出异常
     except requests.RequestException as err:
-        print("Oops: Something Else Happened",err)
+        print(f"请求失败: {err}")
         return
+
     try:
-        res_json = res.json()
+        result = response.json()
     except ValueError:
-        print("Response content is not valid JSON")
+        print("响应内容不是有效的JSON格式")
         return
 
-    total_pics = res_json['result']['pics_total']
-    camer = res_json['result']['pics_array'][0]['camer']
+    if 'result' not in result:
+        print("未找到图片数据")
+        return
 
-    i = total_pics + 1
-    j = 0
-    for pic in res_json['result']['pics_array']:
+    total_pics = result['result']['pics_total']
+    photographer = result['result']['pics_array'][0]['camer']
+
+    print(f"开始下载图片，共 {total_pics} 张，摄影师: {photographer}")
+
+    downloaded_count = 0
+    for pic in result['result']['pics_array']:
+        image_url = "https:" + pic['origin_img']
         image_name = pic['pic_name']
-        download_all_images(("https:" + pic['origin_img']),image_path,image_name)
-        i = i - 1
-        j = j + 1
-        print("正在下栽第{}张 - 文件名:{}".format(i,image_name))
-    print("Total Photos:{} - Downloaded:{} - Photographer:{}".format(total_pics,j,camer))
+        if download_image(image_url, save_path, image_name):
+            downloaded_count += 1
+            print(f"已下载第 {downloaded_count} 张图片: {image_name}")
 
-# 下载图片
-def download_all_images(url,image_path,image_name):
+    print(f"下载完成，共下载 {downloaded_count} 张图片")
+
+# 下载单张图片
+def download_image(url, save_path, file_name):
     try:
         response = requests.get(url, stream=True)
         response.raise_for_status()  # 如果响应状态不是200，就主动抛出异常
     except requests.RequestException as err:
-        print("Oops: Something Else Happened",err)
+        print(f"下载失败: {err}")
+        return False
+
+    time.sleep(2)  # 防止请求过于频繁
+    with open(os.path.join(save_path, file_name), 'wb') as file:
+        file.write(response.content)
+    return True
+
+# 主函数
+def main():
+    activity_no = input("请输入 photoplus 活动编号 (例如: 87654321): ")
+    if not activity_no.isnumeric():
+        print("活动编号输入错误，请输入数字")
         return
-    time.sleep(2)
-    with open(os.path.join(image_path, image_name), 'wb') as out_file:
-        out_file.write(response.content)
 
-id = input("Enter photoplus ID (eg:87654321): ")
-count = input("Enter number of photos (Default:9999): ")
-place = input("Enter where will you go: ")
+    save_path = input("请输入图片保存路径 (例如: ../Pics/): ")
+    if not save_path:
+        print("保存路径不能为空")
+        return
 
-if count.isnumeric():
-  data['count'] = int(count)
-if id.isnumeric():
-  get_all_images(int(id),place)
-else:
-  print('Wrong ID')
+    get_all_images(int(activity_no), save_path)
+
+if __name__ == "__main__":
+    main()
